55f2e31f8d70835d0f4c5f04afe4d60f32b91d6b
Tamerlan Khutuev <t.khutuev@gmail.com>
2022-04-12 16:21:04 +0300

    change gateway

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
+import { ChatModule } from './chat/chat.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GoogleAuthModule } from './google-auth/google-auth.module';
-import { ChatModule } from './chat/chat.module';
 import { RoomModule } from './room/room.module';
 import { MessageModule } from './message/message.module';
 @Module({
--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ChatController } from './chat.controller';
 import { ChatGateway } from './chat.gateway';
 import { ChatService } from './chat.service';
 
 @Module({
-  controllers: [ChatController, ChatGateway],
+  controllers: [ChatGateway],
   providers: [ChatService],
 })
 export class ChatModule {}
