143901a9283a531bc2d88b1720752d05021590cb
Tamerlan Khutuev <t.khutuev@gmail.com>
2022-04-12 10:58:42 +0300

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { GoogleAuthModule } from './google-auth/google-auth.module';
-import { ChatModule } from './chat/chat.module';
-import { RoomModule } from './room/room.module';
-import { MessageModule } from './message/message.module';
 import { config } from 'dotenv';
 
 import { MongooseModule } from '@nestjs/mongoose';
 
 config();
 @Module({
-  imports: [
-    GoogleAuthModule,
-    ChatModule,
-    RoomModule,
-    MessageModule,
-    MongooseModule.forRoot(process.env.MONGODB_URL),
-  ],
+  imports: [MongooseModule.forRoot(process.env.MONGODB_URL)],
   controllers: [AppController],
   providers: [AppService],
 })
