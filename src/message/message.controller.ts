3c5d3d7e528921563fcc3d8c99112d7198a5f31b
GeorgiyEloev <geloev.exceedteam@gmail.com>
2022-04-27 16:39:47 +0300

    add service for room and fix messege socket

--- src/message/message.controller.ts
+++ src/message/message.controller.ts
@@ -1,49 +1,47 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Patch,
-  Post,
-  Res,
-} from '@nestjs/common';
-import { Request, Response } from 'express';
+import { Controller, Delete, Get, Patch, Post, Res } from '@nestjs/common';
+import { Response } from 'express';
+
 import { MessageService } from './message.service';
 
 @Controller('message')
 export class MessageController {
-  // constructor(private readonly messageService: MessageService) {}
-  // @Get('roomMessages')
-  // getAllRoomMessage(@Res() res: Response) {
-  //   return this.messageService.getAllRoomMessage(
-  //     '626696098f5928e2635222b9',
-  //     res,
-  //   );
-  // }
-  // @Post('add')
-  // add(@Res() res: Response) {
-  //   return this.messageService.add(
-  //     '625fdff2d718055dc4ee6ad9',
-  //     '626696098f5928e2635222b9',
-  //     '11111111',
-  //     res,
-  //   );
-  // }
-  // @Patch('change')
-  // change(@Res() res: Response) {
-  //   return this.messageService.change(
-  //     '626696f91e6ec4bd322e117d',
-  //     '625fdff2d718055dc4ee6ad9',
-  //     'test2',
-  //     res,
-  //   );
-  // }
-  // @Delete('delete')
-  // delete(@Res() res: Response) {
-  //   return this.messageService.delete(
-  //     '626696f91e6ec4bd322e117d',
-  //     '625fdff2d718055dc4ee6ad9',
-  //     res,
-  //   );
-  // }
+  constructor(private readonly messageService: MessageService) {}
+  @Get('roomMessages')
+  async getAllRoomMessage(@Res() res: Response) {
+    return res.send(
+      await this.messageService.getAllRoomMessage('626696098f5928e2635222b9'),
+    );
+  }
+
+  @Post('add')
+  async add(@Res() res: Response) {
+    return res.send(
+      await this.messageService.add(
+        '625fdff2d718055dc4ee6ad9',
+        '626696098f5928e2635222b9',
+        '11111111',
+      ),
+    );
+  }
+
+  @Patch('change')
+  async change(@Res() res: Response) {
+    return res.send(
+      await this.messageService.change(
+        '626696f91e6ec4bd322e117d',
+        '625fdff2d718055dc4ee6ad9',
+        'test2',
+      ),
+    );
+  }
+
+  @Delete('delete')
+  async delete(@Res() res: Response) {
+    return res.send(
+      await this.messageService.delete(
+        '626696f91e6ec4bd322e117d',
+        '625fdff2d718055dc4ee6ad9',
+      ),
+    );
+  }
 }
